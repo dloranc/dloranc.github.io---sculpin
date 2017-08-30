2f218257959bb0d8a03f2a573afdcbb22daacad2
Dawid Loranc <dawidloranc@gmail.com>
2017-08-30 21:07:17 +0200

    Remove generate.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ SOURCE_BRANCH="master"
 TARGET_BRANCH="master"
 
 function generate {
-  ./generate.sh
+    php bin/sculpin generate --env=prod
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
--- generate.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-php bin/sculpin generate --env=prod
